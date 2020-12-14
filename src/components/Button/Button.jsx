36ae4df76166ada40aa3c211594f1c033d730db5
o-naichenko <o.naichenko@gmail.com>
2020-12-14 23:59:56 +0200

    Додає файли компонентів, дефолтний експорт

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Button() {
+  window.scrollTo({
+    top: document.documentElement.scrollHeight,
+    behavior: 'smooth',
+  });
+  return <button type="button"></button>;
+}
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ImageGallery() {
+  return (
+    <ul className="ImageGallery">{/* <!-- Набір <li> з зображеннями --> */}</ul>
+  );
+}
